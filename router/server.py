67e4ee4feb2fab833195072528e752a2329863d1
Ian Kirkpatrick <iankirkpatrick@Ians-MacBook-Pro-3.local>
2025-02-02 19:13:17 -0700

    week 4

--- router/server.py
+++ router/server.py
@@ -5,6 +5,7 @@ from .core import router as core_router
 from .week1 import router as week1_router
 from .week2 import router as week2_router
 from .week3 import router as week3_router
+from .week4 import router as week4_router
 
 # global route collection
 api_router = APIRouter(default_response_class=JSONResponse)
@@ -12,3 +13,4 @@ api_router.include_router(core_router, prefix="", tags=[])
 api_router.include_router(week1_router, prefix="/week1", tags=["Week 1"])
 api_router.include_router(week2_router, prefix="/week2", tags=["Week 2"])
 api_router.include_router(week3_router, prefix="/week3", tags=["Week 3"])
+api_router.include_router(week4_router, prefix="/week4", tags=["Week 4"])
